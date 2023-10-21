8039f9f848fc74a82a8ba1e9cbe99f4e1b530fca
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-21 19:49:09 +0100

    #

--- script1.js
+++ script1.js
@@ -573,3 +573,27 @@ function checkSign(num){
   : "zero";
 }
 console.log(checkSign(10));
+
+// Use Recursion to Create a Countdown
+function countdown(n){
+  if (n < 1){
+    return [];
+  }else{
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+console.log(countdown(5));
+
+// Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum){
+  if (startNum === endNum){
+    return [startNum];
+  }else{
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+console.log(rangeOfNumbers(1, 5));
